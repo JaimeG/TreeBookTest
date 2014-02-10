33b8e4678d236085e5795c7aa74aa63033b119e0
Jaime Garay <jaime@joystickinteractive.com>
2014-02-10 01:23:14 -0600

    Created a Rails application and added a new cositos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '21328116ccaaa3624a08f2f3984d994c63a8b1cb6b6ea07cbe9ab7c13ce86bc085f5de030d06221f3eca7ef75fb0613f61c23d988f113517c0976fc1de2e1d60'
